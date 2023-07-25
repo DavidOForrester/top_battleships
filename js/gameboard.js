99cc396673bb0f7267f6a324a5e80cee2d71a599
David Forrester <david.o.forrester@gmail.com>
2023-07-25 20:54:32 +1000

    building out the coordinate mapping function

--- js/gameboard.js
+++ js/gameboard.js
@@ -5,19 +5,41 @@ const Gameboard = () => {
   const battleship = Ship(4, 0);
   const cruiser = Ship(3, 0);
   const submarine = Ship(3, 0);
+  const destroyer = Ship(2, 0);
 
-  const placeShip = (shipType, coordinateStart, corrdinateEnd) => {
-    if (shipType == "destroyer") {
-      const destroyer = Ship(2, 0);
+  let carrierCoord = [];
+  let battleshipCoord = [];
+  let cruiserCoord = [];
+  let submarineCoord = [];
+  let destoryerCoord = [];
+
+  const placeShip = (shipType, coordStart, corrdEnd) => {
+    let coordArray = [];
+    if (coordStart[0] == corrdEnd[0]) {
+      for (let i = coordStart[1]; i <= corrdEnd[1]; i++) {
+        coordArray.push([coordStart[0], i]);
+      }
+    } else {
+      for (let i = coordStart[0]; i <= corrdEnd[0]; i++) {
+        coordArray.push([i, coordStart[1]]);
+      }
+    }
+
+    if (shipType == "carrier") {
+      carrierCoord = coordArray;
+    } else if (shipType == "battleship") {
+      battleshipCoord = coordArray;
+    } else if (shipType == "cruiser") {
+      cruiserCoord = coordArray;
+    } else if (shipType == "submarine") {
+      submarineCoord = coordArray;
+    } else if (shipType == "destoryer") {
+      destoryerCoord = coordArray;
     }
   };
 
-  const receiveAttack = (coordinates) => {};
-  return { placeShip, receiveAttack };
+  const receiveAttack = (coord) => {};
+  return { placeShip, receiveAttack, destoryerCoord };
 };
-
-const test = Gameboard();
-
-test.placeShip("destoryer", [0,0], [1,0])
 
 module.exports = Gameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,1 +1,13 @@
 const Gameboard = require("../js/gameboard");
+
+test("ship coordinate array", () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip("destoryer", [5, 3], [5, 7]);
+  expect(gameboard.destoryerCoord).toStrictEqual([
+    [5, 3],
+    [5, 4],
+    [5, 5],
+    [5, 6],
+    [5, 7],
+  ]);
+});
