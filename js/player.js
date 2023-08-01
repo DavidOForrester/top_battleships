bf6d82afef84866103833fa079fc00b765680410
David Forrester <david.o.forrester@gmail.com>
2023-08-01 19:41:49 +1000

    AI generates random attacks

--- js/player.js
+++ js/player.js
@@ -1,7 +1,25 @@
-const Gameboard = require("../js/gameboard")
+const Gameboard = require("../js/gameboard");
 
 const Player = () => {
+  let gameboard = Gameboard();
 
-}
+  const makeMove = (type, coord) => {
+    if (type == "AI") {
+      //random generate attack
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
 
-modeule.exports = Player
+      gameboard.receiveAttack([x, y]);
+    } else {
+      gameboard.receiveAttack(coord);
+    }
+  };
+
+  return { makeMove };
+};
+
+module.exports = Player;
+
+let player1 = Player();
+
+player1.makeMove("AI");
