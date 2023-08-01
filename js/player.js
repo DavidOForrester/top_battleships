719ca9f507607653e0c03f759ffd59288093efbb
David Forrester <david.o.forrester@gmail.com>
2023-08-01 19:50:17 +1000

    Added check for gameover

--- js/player.js
+++ js/player.js
@@ -3,23 +3,29 @@ const Gameboard = require("../js/gameboard");
 const Player = () => {
   let gameboard = Gameboard();
 
-  const makeMove = (type, coord) => {
+  const receiveMove = (type, coord) => {
     if (type == "AI") {
       //random generate attack
       let x = Math.floor(Math.random() * 10);
       let y = Math.floor(Math.random() * 10);
 
-      gameboard.receiveAttack([x, y]);
+      let gameOverCheck = gameboard.receiveAttack([x, y]);
     } else {
-      gameboard.receiveAttack(coord);
+      let gameOverCheck = gameboard.receiveAttack(coord);
+    }
+
+    if(gameOverCheck == "gameover") {
+      return gameOverCheck
     }
   };
 
-  return { makeMove };
+  return { receiveMove };
 };
 
 module.exports = Player;
 
 let player1 = Player();
+let AIplayer = Player();
 
-player1.makeMove("AI");
+player1.receiveMove("AI");
+AIplayer.receiveMove([0, 0]);
