a833ef1854f9dd4481b47808b27777e8104a5bd5
David Forrester <david.o.forrester@gmail.com>
2023-08-03 21:19:08 +1000

    logic to check if the guess has already been made

--- js/player.js
+++ js/player.js
@@ -2,22 +2,33 @@ const Gameboard = require("../js/gameboard");
 
 const Player = () => {
   let gameboard = Gameboard();
-  let gameOverCheck = null
+  let gameOverCheck = null;
 
   const receiveMove = (type, coord) => {
+    let match = "No";
     if (type == "AI") {
-      //random generate attack
       let x = Math.floor(Math.random() * 10);
       let y = Math.floor(Math.random() * 10);
 
-      gameOverCheck = gameboard.receiveAttack([x, y]);
-    } else {
+      coord = [x, y];
+    }
+    
+    let guesses = gameboard.missedCoord.concat(gameboard.hitCoord);
+
+    for (let i = 0; i < guesses.length; i++) {
+      if (guesses[i][0] == coord[0] && guesses[i][1] == coord[1]) {
+        match = "Yes";
+      }
+    }
+
+    if (match == "No") {
       gameOverCheck = gameboard.receiveAttack(coord);
     }
 
-    if(gameOverCheck == "gameover") {
-      return gameOverCheck
+    if (gameOverCheck == "gameover") {
+      return gameOverCheck;
     }
+    console.log(match)
   };
 
   return { receiveMove };
@@ -28,5 +39,9 @@ module.exports = Player;
 let player1 = Player();
 let AIplayer = Player();
 
+/*player1.receiveMove("AI");
 player1.receiveMove("AI");
-AIplayer.receiveMove([0, 0]);
+player1.receiveMove("AI");
+player1.receiveMove("AI");*/
+AIplayer.receiveMove(null, [0, 0]);
+AIplayer.receiveMove(null, [0, 0]);
