dd3322e153393579f4cfd2380d292840f70110bb
David Forrester <david.o.forrester@gmail.com>
2023-08-06 15:32:00 +1000

    created gameboards

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,10 @@
 export function pageLoad() {
+  //game div
   const game = document.createElement("div");
   game.id = "game";
   document.body.appendChild(game);
 
+  //structure divs
   const header = document.createElement("div");
   header.id = "header";
   header.innerText = "Battleships"
@@ -15,4 +17,31 @@ export function pageLoad() {
   const footer = document.createElement("div");
   footer.id = "footer";
   game.appendChild(footer);
+
+  //divs within the main
+  const myGameboard = document.createElement("div")
+  myGameboard.id = "my-gameboard"
+  main.appendChild(myGameboard)
+
+  for (let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++) {
+      const cell = document.createElement("div")
+      cell.className = i + ", " + j
+      cell.id = "game-cell"
+      myGameboard.appendChild(cell)
+    }
+  }
+
+  const enemyGameboard = document.createElement("div")
+  enemyGameboard.id = "enemy-gameboard"
+  main.appendChild(enemyGameboard)
+
+  for (let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++) {
+      const cell = document.createElement("div")
+      cell.className = i + ", " + j
+      cell.id = "game-cell"
+      enemyGameboard.appendChild(cell)
+    }
+  }
 }
